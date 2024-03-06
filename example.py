1af4338be5d0b301627de86aebb8c717263befa6
Kye <kye@apacmediasolutions.com>
2024-03-05 22:29:28 -0800

    [README]

--- example.py
+++ example.py
@@ -1,1 +1,27 @@
-g
+import torch
+from vision_mamba.model import Vim
+
+# Create a random tensor
+x = torch.randn(1, 3, 224, 224)
+
+# Create an instance of the Vim model
+model = Vim(
+    dim=256,
+    heads=8,
+    dt_rank=32,
+    dim_inner=256,
+    d_state=256,
+    num_classes=1000,
+    image_size=224,
+    patch_size=16,
+    channels=3,
+    dropout=0.1,
+    depth=12,
+)
+
+# Perform a forward pass through the model
+out = model(x)
+
+# Print the shape and output of the forward pass
+print(out.shape)
+print(out)
